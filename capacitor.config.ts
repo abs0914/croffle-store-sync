ccf771066bcde8556f76dc33824f2ca9454f79f4
abs0914 <alrsantiago@gmail.com>
2025-07-31 11:10:43 +0800

    latest updates

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.croffle.storesync',
+  appName: 'Croffle Store Sync',
+  webDir: 'dist'
+};
+
+export default config;
